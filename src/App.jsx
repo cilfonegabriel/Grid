e0012e454ddfac528286d5cc6d2f62390d8900c3
gabicilfone <cilfonegabriel@gmail.com>
2023-06-04 22:07:29 -0300

    Refactoring code for App

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,42 @@
 import "./App.css"
 import { TwitterFollowCard } from "./TwitterFollowCard"
 
+const users = [
+    {
+        userName: 'Bizarrap',
+        name: 'Bizarrap',
+        isFollowing: false,
+    },
+    {
+        userName: 'Indie5051',
+        name: 'Indie 505 ',
+        isFollowing: false,
+    },
+    {
+        userName: 'MrAngeloValdes',
+        name: 'Will',
+        isFollowing: false,
+    },    {
+        userName: 'Messismo10',
+        name: 'Messismo',
+        isFollowing: false,
+    }
+]
+
 export function App () {
-    
-return (
-    <section className="App">
-        <TwitterFollowCard  userName="Bizarrap" >
-            Bizarrap 
-        </TwitterFollowCard> 
-        <TwitterFollowCard  userName="Indie5051" >
-        Indie 505 
-        </TwitterFollowCard> 
-    </section>
+    return (
+        <section className="App">
+            {
+                users.map(({ userName, name, isFollowing }) => (
+                    <TwitterFollowCard
+                        key={userName}
+                        userName={userName}
+                        initialIsFollowing={isFollowing}
+                    >
+                        {name}
+                    </TwitterFollowCard> 
+                ))
+            }
+        </section>
     )
 }
